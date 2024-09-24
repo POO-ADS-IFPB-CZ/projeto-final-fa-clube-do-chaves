91b14086363cd892515fb91693c05a2e81f808dc
ericksjp <ericksjp703@gmail.com>
2024-09-24 15:07:46 -0300

    Retira target do gitgnore

--- src/main/java/com/poo/aluger/gui/Main.java
+++ src/main/java/com/poo/aluger/gui/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
   public void start(Stage stage) throws IOException {
     FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("TelaLogin.fxml"));
     Scene scene = new Scene(fxmlLoader.load());
-    stage.setTitle("Dashboard");
+    stage.setTitle("TelaLogin");
     stage.setScene(scene);
     stage.show();
   }
